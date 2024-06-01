04488ed90b358b39371076c38fd87b90c6243b31
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-05-31 21:55:22 -0300

    feat: form base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import { Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
+    <Container maxWidth="md">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Formulário com React e TypeScript
+        </Typography>
+        <Form />
       </header>
-    </div>
+    </Container>
   );
 }
 
